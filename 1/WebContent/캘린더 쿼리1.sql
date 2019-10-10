f2129d8a8bbc317e6b77008d0fd2b82e7ad89e8d
sanghyukYoun <tkdgur12347@naver.com>
2019-10-10 12:41:37 +0900

    캘린더쿼리 주석 수정

--- 1/WebContent/캘린더 쿼리1.sql
+++ 1/WebContent/캘린더 쿼리1.sql
@@ -1,14 +1,14 @@
 
 -- root 로 접속
 
--- jspbook 사용자계정 생성
+-- Calendar 사용자계정 생성
 create user 'Calendar'@'localhost' identified by '1234';
 
--- jspbook이 사용하는 DB(Schema) 생성
+-- Calendar이 사용하는 DB(Schema) 생성
 create database Calendar;
 
--- jspbook에게 jspdb 권한 부여
+-- Calendar에게 Calendar(스키마) 권한 부여
 grant all privileges on Calendar.* to 'Calendar'@'localhost' with grant option;
 
--- jspbook에게 파일 처리권한 부여
+-- Calendar에게 파일 처리권한 부여
 grant file on *.* to 'Calendar'@'localhost';
