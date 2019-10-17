b478788f884f061715055204156f823d017b307c
sanghyukYoun <tkdgur12347@naver.com>
2019-10-17 12:23:05 +0900

    캘린더 db와 스키마에 차이를 줌

--- 1/WebContent/캘린더 쿼리1.sql
+++ 1/WebContent/캘린더 쿼리1.sql
@@ -5,10 +5,10 @@
 create user 'Calendar'@'localhost' identified by '1234';
 
 -- Calendar이 사용하는 DB(Schema) 생성
-create database Calendar;
+create database Calendardb;
 
 -- Calendar에게 Calendar(스키마) 권한 부여
-grant all privileges on Calendar.* to 'Calendar'@'localhost' with grant option;
+grant all privileges on Calendardb.* to 'Calendar'@'localhost' with grant option;
 
 -- Calendar에게 파일 처리권한 부여
 grant file on *.* to 'Calendar'@'localhost';
